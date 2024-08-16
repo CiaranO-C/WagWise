cdd52d9ed9ed742040f19a0bf495fb03f92b7762
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-16 13:49:44 +0100

    insert service functions into handlers

--- src/article/article-service.js
+++ src/article/article-service.js
@@ -1,15 +1,34 @@
-const prisma = require('../../config/prisma');
+const prisma = require("../../config/prisma");
 
 async function fetchAllArticles() {
   /* db query for all articles */
+  try {
+    const articles = await prisma.article.findMany();
+  } catch (error) {
+    
+  }
 }
 
 async function fetchArticleById(id) {
   /* db query for single article */
+  try {
+    const article = await prisma.article.findUnique({ where: { id: id } });
+  } catch (error) {
+    
+  }
 }
 
-async function insertComment() {
+async function insertComment(articleId, comment) {
   /* db query to insert new comment */
+  try {
+    const { authorId, text } = comment;
+    const newComment = await prisma.comment.create({
+      data: { articleId, authorId, text },
+    });
+  } catch (error) {
+    
+  }
+
 }
 
 module.exports = { fetchAllArticles, fetchArticleById, insertComment };
