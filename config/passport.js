f720555d0eb8955d37463b81e24ecf0e9c4794ef
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-16 13:52:30 +0100

    remove redundant imports, remove token creation from local strategy

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,6 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const bcrypt = require("bcryptjs");
 const prisma = require("./prisma");
-const jwt = require("jsonwebtoken");
 
 function intialisePassport() {
   passport.use(
@@ -33,7 +32,7 @@ function intialisePassport() {
         } catch (error) {
           // if error log and return error
           console.error(error);
-          return done(error, false, { message: "Error during authentication"});
+          return done(error, false, { message: "Error during authentication" });
         }
       },
     ),
@@ -59,12 +58,7 @@ function intialisePassport() {
         }
 
         // Correct details entered, return user
-        const token = jwt.sign(
-          { userId: user.id },
-          process.env.JWT_PRIVATE_KEY,
-          { algorithm: "HS384", expiresIn: "15s" },
-        );
-        return done(null, user, { message: "Success, token granted", token });
+        return done(null, user);
       } catch (error) {
         console.error(error);
         done(error);
