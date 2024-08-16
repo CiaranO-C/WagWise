80f693256b94c801345e7373ea8807c34e118c2b
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-16 13:47:42 +0100

    write getArticle handler

--- src/article/article-handler.js
+++ src/article/article-handler.js
@@ -1,15 +1,37 @@
 const service = require("./article-service");
 
 async function getAllArticles(req, res, next) {
-  /* logic - service.fetchAllArticles */
+  try {
+    const articles = await service.fetchAllArticles();
+    if (!articles) {
+      return res.status(200).json({
+        message: "No articles found",
+      });
+    }
+    return res.status(200).json({ articles });
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function getArticle(req, res, next) {
-  /* logic - service.fetchArticleById */
+  const id = Number(req.params.id);
+  try {
+    const article = await service.fetchArticleById(id);
+    if (!article) {
+      return res.status(200).json({
+        message: "Article not found",
+      });
+    }
+    return res.status(200).json({ article });
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function postComment(req, res, next) {
   /* logic - service.insertComment */
+  res.json({ message: "you are authorized to comment!" });
 }
 
 module.exports = { getAllArticles, getArticle, postComment };
