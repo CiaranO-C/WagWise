bf89adabae570d94188abcfe88d1694b2e0c0097
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-16 13:49:02 +0100

    correct require path

--- src/article/article-router.js
+++ src/article/article-router.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const handler = require("./article-handler");
+const { userAuth } = require('../auth/auth');
 
 const articleRouter = Router();
 
+// base --> "/articles"
+
 articleRouter.get("/", handler.getAllArticles);
 articleRouter.get("/:id", handler.getArticle);
-articleRouter.post("/:id/comment", handler.postComment);
+articleRouter.post("/:id/comment", userAuth, handler.postComment);
 
 module.exports = articleRouter;
