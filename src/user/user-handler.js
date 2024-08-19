b803e95ab3014afb7a4e4d73abba092ba6ad0526
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-19 11:16:38 +0100

    write comment handlers, update user handler

--- src/user/user-handler.js
+++ src/user/user-handler.js
@@ -1,7 +1,12 @@
 const { validationResult } = require("express-validator");
 const { createNewUser } = require("./user-service");
+const {
+  retrieveComments,
+  deleteDbComment,
+  updateUser,
+} = require("./user-queries");
 
-const createUserHandler = async (req, res, next) => {
+const createUser = async (req, res, next) => {
   try {
     const { username, password } = req.body;
 
@@ -19,4 +24,60 @@ const createUserHandler = async (req, res, next) => {
   }
 };
 
-module.exports = { createUserHandler };
+async function getUserComments(req, res, next) {
+  try {
+    const currentUserId = req.user.id;
+    const id = Number(req.params.id);
+    if (currentUserId !== id)
+      return res.status(403).json({ message: "forbidden" });
+    const comments = await retrieveComments(id);
+    res.json({ comments });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteComment(req, res, next) {
+  try {
+    const currentUserId = req.user.id;
+    const { userId, commentId } = req.params;
+    if (currentUserId !== Number(userId)) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    const deleted = await deleteDbComment(commentId);
+    res.json({ deleted });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function adminDeleteComment(req, res, next) {
+  try {
+    const id = Number(req.params.id);
+    const deleted = await deleteDbComment(id);
+    res.json({ deleted });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function putUser(req, res, next) {
+  try {
+    currentUserId = req.user.id;
+    const { id } = req.params;
+    if (currentUserId !== id)
+      return res.status(403).json({ message: "Forbidden" });
+    const updated = await updateUser(id);
+    res.json({ updated });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  createUser,
+  getUserComments,
+  deleteComment,
+  adminDeleteComment,
+  putUser,
+};
