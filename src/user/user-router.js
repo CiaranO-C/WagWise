99c0cb05680a32b1651a30f0c86fd35dfb18f16b
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-19 11:18:45 +0100

    write protected user/admin routes

--- src/user/user-router.js
+++ src/user/user-router.js
@@ -3,25 +3,40 @@ const {
   validateNewUsername,
   validateNewPassword,
 } = require("./user-validation");
-const { createUserHandler } = require("./user-handler");
+const handler = require("./user-handler");
 const { auth, tokens } = require("./auth");
+const { userAuth, adminAuth } = require("./auth/auth");
 
 const userRouter = Router();
 
 // base --> "/user"
 
+/* Public */
 userRouter.post(
   "/sign-up",
   validateNewUsername,
   validateNewPassword,
-  createUserHandler,
+  handler.createUser,
 );
 
 userRouter.post("/log-in", auth.loginAuth, tokens.sendTokensToClient);
+
 userRouter.get(
   "/refresh-token",
   tokens.authorizeRefreshToken,
   tokens.sendTokensToClient,
 );
+
+/* User */
+userRouter.get("/:id/comments", userAuth, handler.getUserComments);
+userRouter.delete(
+  ":userId/comments/:commentId",
+  userAuth,
+  handler.deleteComment,
+);
+userRouter.put("/:id", userAuth, handler.putUser);
+
+/* Admin */
+userRouter.delete("/comments/:id", adminAuth, handler.deleteComment);
 
 module.exports = userRouter;
