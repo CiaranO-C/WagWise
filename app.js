4ca55bf92fc5c4294528b1b8bcb16f3270c805f2
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-09-02 21:42:37 +0100

    add express cors middleware, more detailed

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const express = require("express");
 const { intialisePassport } = require("./config/passport");
 const apiRouter = require("./src/apiRouter");
 const cookieParser = require("cookie-parser");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true, 
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
