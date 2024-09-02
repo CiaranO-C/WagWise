9ea0e748dd0209f172656999f0252ba08b07e0f2
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-09-02 21:41:48 +0100

    add user to json response, move sav refresh token into queries file

--- src/user/auth/tokens.js
+++ src/user/auth/tokens.js
@@ -1,12 +1,13 @@
 const jwt = require("jsonwebtoken");
 const { v4: uuid } = require("uuid");
-const { saveRefreshToken, checkValidToken } = require("./auth-service");
+const { checkValidToken } = require("./auth-service");
+const { saveRefreshToken } = require("./auth-queries");
 
 async function generateTokens(userId) {
   try {
     const accessToken = jwt.sign({ userId }, process.env.JWT_PRIVATE_KEY, {
       algorithm: "HS384",
-      expiresIn: "20s",
+      expiresIn: "1h",
     });
 
     const jti = uuid();
@@ -14,13 +15,13 @@ async function generateTokens(userId) {
     const created = new Date();
     // 7 day expiry
     const expires = new Date(created.getTime() + 7 * 24 * 60 * 60 * 1000);
+
     await saveRefreshToken(userId, jti, created, expires);
 
     const refreshToken = jwt.sign({ jti }, process.env.JWT_REFRESH_KEY, {
       algorithm: "HS384",
       expiresIn: "7d",
     });
-
     return [accessToken, refreshToken];
   } catch (error) {
     throw new Error("Failed to generate token pair");
@@ -30,16 +31,25 @@ async function generateTokens(userId) {
 async function sendTokensToClient(req, res, next) {
   try {
     // generate new token pair - also saves to db
-    const [accessToken, refreshToken] = await generateTokens(req.user.id);
+    const { user } = req;
+    const [accessToken, refreshToken] = await generateTokens(user.id);
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      secure: process.env.APP_ENV === "production", // ensures https only in production
+      secure: "true",
     });
 
-    return res.status(200).json({ jwt: accessToken });
+    return res.status(200).json({
+      jwt: accessToken,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
   } catch (error) {
     next(error);
   }
