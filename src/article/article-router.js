1edcd9d4fe1c5203e6d6418860348e929301ec37
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-19 23:06:48 +0100

    write route handler for admin search

--- src/article/article-router.js
+++ src/article/article-router.js
@@ -23,6 +23,7 @@ articleRouter.post("/", handler.postArticle);
 articleRouter.post("/:id/publish", handler.postPublish);
 articleRouter.post("/:id/unpublish", handler.postUnpublish);
 articleRouter.get("/admin/unpublished", handler.getUnpublishedArticles);
+articleRouter.get("/admin/search", handler.getSearchArticles);
 articleRouter.put("/:id", handler.putArticle);
 articleRouter.delete("/:id", handler.deleteArticle);
 
