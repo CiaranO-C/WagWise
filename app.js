770ed001adb9cf3685634487bbc31084929d3053
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-07 11:02:40 +0000

    remove config from file

--- app.js
+++ app.js
@@ -1,43 +1,5 @@
-require("dotenv").config();
-const express = require("express");
-const { intialisePassport } = require("./config/passport");
-const apiRouter = require("./src/apiRouter");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-
-const app = express();
+const app = require("./config/app-config");
 
 const PORT = process.env.PORT || 5000;
-
-const allowedOrigins = [
-  "http://localhost:5174",
-  "https://wagwise-cms.vercel.app",
-  "https://wagwise-blog.vercel.app",
-];
-
-app.set("trust proxy", true);
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      console.log("Origin received:", origin);
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.log(origin);
-
-        callback(new Error("Invalid origin"));
-      }
-    },
-    credentials: true,
-  }),
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-intialisePassport();
-
-app.use("/api", apiRouter);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
