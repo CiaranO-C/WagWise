f206f39089063232f9ac12baf4338f17af278746
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-09 00:09:58 +0000

    write test util functions

--- tests/utils.js
+++ tests/utils.js
@@ -1,3 +1,8 @@
+const prisma = require("../config/prisma");
+const { createArticle } = require("../src/article/article-queries");
+const { generateTokens } = require("../src/user/auth/tokens");
+const { hashPassword } = require("../src/user/user-utils");
+
 function getRandomUsername() {
   return `testUser${Math.floor(Math.random() * 100000)}`;
 }
@@ -11,10 +16,37 @@ function testUser() {
   };
 }
 
-const testArticle = {
-  title: "Title",
-  text: "<p>text</p>",
-  tagNames: ["tag1", "tag2"],
-};
+async function createTestUser(role = "USER") {
+  const { username, password } = testUser();
+  const hashedPassword = await hashPassword(password);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      password: hashedPassword,
+      role,
+    },
+  });
 
-module.exports = { testUser, testArticle };
+  const [token] = await generateTokens(user.id);
+
+  return { user, token };
+}
+
+async function createTestArticle(authorId) {
+  const testArticle = {
+    title: "Title",
+    text: "<p>text</p>",
+    tagNames: ["tag1", "tag2"],
+  };
+
+  const article = await createArticle(
+    testArticle.title,
+    testArticle.text,
+    testArticle.tagNames,
+    authorId,
+  );
+
+  return article;
+}
+
+module.exports = { testUser, createTestUser, createTestArticle };
