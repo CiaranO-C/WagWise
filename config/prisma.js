0e28b7a42999199b269fd70314dc513647879f04
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-09 15:01:46 +0000

    add databaseURL condition based on node_env

--- config/prisma.js
+++ config/prisma.js
@@ -1,5 +1,19 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const env = process.env.NODE_ENV;
+const databaseUrl =
+  env === "test"
+    ? process.env.TEST_DATABASE_URL
+    : env === "development"
+      ? process.env.DATABASE_PUBLIC_URL
+      : process.env.DATABASE_URL;
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
 
 module.exports = prisma;
